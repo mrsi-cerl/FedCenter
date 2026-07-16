136d61816fabf9644f8749a1376830126c33437a
DanBriggs <daniel.t.briggs@erdc.dren.mil>
2026-07-16 15:16:44 -0500

    scss recompiled

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,14 @@ const uswds = require("@uswds/compile");
 uswds.settings.version = 3;
 
 /**
+ * Paths
+ * dist refers to where we compile the css to
+ * see https://designsystem.digital.gov/documentation/getting-started/developers/phase-two-compile/#step-5-customize-path-settings-2
+ */
+uswds.paths.dist.img = "src/components/uswds/img";
+uswds.paths.dist.css = "src/components/uswds/css";
+
+/**
  * Exports
  * Add as many as you need
  */
